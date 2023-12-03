54f60008f2ce697b54800422e652194d8dfde201
Satria Ardiansyah <108807878+satriaardiansyah@users.noreply.github.com>
2023-12-03 22:33:50 +0700

    Update db.js
    ganti db

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,17 @@
 const mysql = require('mysql2');
 
 const dbPool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: 'sql12.freesqldatabase.com',
+    user: 'sql12667163',
+    password: 'Qe9bpnzZEX',
+    database: 'sql12667163',
 });
+// const dbPool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+// });
 
 // const dbPool = mysql.createPool({
 //     host: process.env.DB_HOST,
@@ -16,9 +22,3 @@ const dbPool = mysql.createPool({
 
 module.exports = dbPool.promise();
 
-// const dbPool = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'gonusa-test',
-// });
