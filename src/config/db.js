42c9351b509e5ea7aba4433ee8b9f272a9b77b11
Satria <satriaardiansyah133@gmail.com>
2023-12-04 16:32:01 +0700

    update 1.1

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,5 @@
 const mysql = require('mysql2');
 
-// const dbPool = mysql.createPool({
-//     host: 'sql12.freesqldatabase.com',
-//     user: 'sql12667163',
-//     password: 'Qe9bpnzZEX',
-//     database: 'sql12667163',
-// });
-
 const dbPool = mysql.createPool({
     host: 'sql12.freesqldatabase.com',
     user: 'sql12667163',
@@ -18,6 +11,13 @@ const dbPool = mysql.createPool({
 //     user: process.env.DB_USERNAME,
 //     password: process.env.DB_PASSWORD,
 //     database: process.env.DB_NAME,
+// });
+
+// const dbPool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'gonusa-test',
 // });
 
 
